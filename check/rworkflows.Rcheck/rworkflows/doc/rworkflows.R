a2807630732feb2403652ac594e4da2dbfae0ade
bschilder <34280215+bschilder@users.noreply.github.com>
2022-11-07 02:21:42 +0000

    Deploying to gh-pages from @ neurogenomics/rworkflows@09b995cd7faf93d1e0750ad245181a43eb7a6704 🚀

---
+++ check/rworkflows.Rcheck/rworkflows/doc/rworkflows.R
@@ -0,0 +1,7 @@
+## ---- echo=FALSE, include=FALSE-----------------------------------------------
+pkg <- read.dcf("../DESCRIPTION", fields = "Package")[1]
+library(pkg, character.only = TRUE)
+
+## ----Session Info-------------------------------------------------------------
+utils::sessionInfo()
+
