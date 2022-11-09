43c110c165723129d634f7965bd54ee321e382ee
Brian M. Schilder <34280215+bschilder@users.noreply.github.com>
2022-11-09 16:36:02 +0000

    convert no to false in brackets

--- R/use_workflow.R
+++ R/use_workflow.R
@@ -118,8 +118,8 @@ use_workflow <- function(name="rworkflows",
     dir.create(dirname(path),showWarnings = FALSE, recursive = TRUE)
     messager("Saving workflow ==>",path,v=verbose)
     #### Write bools as true/false rather than yes/no (default) ####
-    handlers2 <- list('bool#yes' = function(x){'true'},
-                      'bool#no' = function(x){'false'})
+    handlers2 <- list('bool#yes' = function(x){"${{ true }}"},
+                      'bool#no' = function(x){enquote("${{ false }}")})
     yml2 <- yaml::yaml.load(yaml::as.yaml(yml), handlers = handlers2)
     yaml::write_yaml(x = yml2,
                      file = path)
