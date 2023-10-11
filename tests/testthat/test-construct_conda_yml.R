fed72f5cd642c695a2d7354da7dcdab06c60595f
bschilder <brian_schilder@alumni.brown.edu>
2023-10-11 17:22:55 +0100

    Test conda functions

--- tests/testthat/test-construct_conda_yml.R
+++ tests/testthat/test-construct_conda_yml.R
@@ -15,16 +15,16 @@ test_that("construct_conda_yml works", {
   
   yml2 <- construct_conda_yml(name="myenv",
                               dependencies=c("anndata>=1.0","scanpy<20.1.1"),
-                              pip = c("pip","mytool1","mtool2"), 
+                              pip = c("mytool1","mytool2"), 
                               return_path = FALSE,
                               save_path = save_path)
-  testthat::expect_true(all(c("name","channels","dependencies","pip") %in% names(yml2)))
+  testthat::expect_true(all(c("name","channels","dependencies") %in% names(yml2)))
+  testthat::expect_true(all(c("mytool1","mytool2") %in% yml2$dependencies[[4]][[1]]))
   
   yml3 <- construct_conda_yml(name="myenv",
                               dependencies=c("anndata","scanpy"),
-                              save_path = NULL,
                               preview = TRUE,
-                              return_path = TRUE,
+                              return_path = FALSE,
                               save_path = save_path)
   testthat::expect_true(all(c("name","channels","dependencies") %in% names(yml3)))
   
@@ -41,7 +41,12 @@ test_that("construct_conda_yml works", {
                                  preview = TRUE,
                                  return_path = TRUE,
                                  save_path = save_path)
-    out <- reticulate::conda_create(environment = path2)
+    testthat::expect_true(file.exists(path2))
+    out <- reticulate::conda_create(environment = path2, 
+                                    envname = envname)
+    testthat::expect_true(file.exists(out))
+    testthat::expect_equal(basename(out),"python")
+    testthat::expect_equal(basename(dirname(dirname(out))),envname)
   }
   
 })
