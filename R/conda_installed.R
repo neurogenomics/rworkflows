7b52c2b1a3a4050efbb720ad572c02e6e65b0d64
Brian M. Schilder <34280215+bschilder@users.noreply.github.com>
2023-10-31 19:06:07 +0000

    Declare conda env var

--- R/conda_installed.R
+++ R/conda_installed.R
@@ -1,6 +1,15 @@
 conda_installed <- function(){
   if(is_gha()){
-    Sys.getenv("CONDA")!="" | Sys.getenv("CONDA_EXE")!=""
+    if(Sys.getenv("CONDA")!=""){
+      message("Using CONDA=",Sys.getenv("CONDA"))
+      return(TRUE)
+    } else if(Sys.getenv("CONDA_EXE")!=""){
+      message("Using CONDA_EXE=",Sys.getenv("CONDA_EXE"))
+      return(TRUE)
+    } else {
+      message("CONDA not found.")
+      return(FALSE)
+    }
     # reticulate::use_miniconda(conda=Sys.getenv("CONDA_EXE"))
   } else {
     requireNamespace("reticulate")
