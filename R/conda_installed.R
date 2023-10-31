273d2a607c9dcb66481494e7ea05e0fc4113e777
Brian M. Schilder <34280215+bschilder@users.noreply.github.com>
2023-10-31 18:51:48 +0000

    test CONDA_EXE

--- R/conda_installed.R
+++ R/conda_installed.R
@@ -1,8 +1,9 @@
 conda_installed <- function(){
-  # if(is_gha()){
-  #   Sys.getenv("CONDA")!=""
-  # } else {
+  if(is_gha()){
+    Sys.getenv("CONDA")!="" | Sys.getenv("CONDA_EXE")!=""
+    # reticulate::use_miniconda(conda=Sys.getenv("CONDA_EXE"))
+  } else {
     requireNamespace("reticulate")
     utils::getFromNamespace("conda_installed","reticulate")()
-  # }
+  }
 }
