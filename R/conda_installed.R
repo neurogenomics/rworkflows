5696dc394e679dd27e4b2701931e1ce1a0011c8f
Brian M. Schilder <34280215+bschilder@users.noreply.github.com>
2023-10-31 19:25:27 +0000

    temp conda fix with Mambaforge

--- R/conda_installed.R
+++ R/conda_installed.R
@@ -1,13 +1,13 @@
-conda_installed <- function(){
+conda_installed <- function(verbose = FALSE){
   if(is_gha()){
     if(Sys.getenv("CONDA")!=""){
-      message("Using CONDA=",Sys.getenv("CONDA"))
+      messager("Using CONDA=",Sys.getenv("CONDA"), v=verbose)
       return(TRUE)
     } else if(Sys.getenv("CONDA_EXE")!=""){
-      message("Using CONDA_EXE=",Sys.getenv("CONDA_EXE"))
+      messager("Using CONDA_EXE=",Sys.getenv("CONDA_EXE"), v=verbose)
       return(TRUE)
     } else {
-      message("CONDA not found.")
+      messager("CONDA not found.", v=verbose)
       return(FALSE)
     }
     # reticulate::use_miniconda(conda=Sys.getenv("CONDA_EXE"))
