de5adfd73f017b20b997313992f2429e7a437451
Hadley Wickham <h.wickham@gmail.com>
2025-12-10 07:43:35 -0600

    Fix for latest yaml package
    This test fails since it now lives in r-lib. I think it's a bit dangerous to rely on other packages like this, so I just removed the test.

--- tests/testthat/test-infer_docker_org.R
+++ tests/testthat/test-infer_docker_org.R
@@ -5,14 +5,7 @@ test_that("infer_docker_org works", {
   out <- infer_docker_org(docker_org = docker_org, 
                           docker_registry = "docker.io")
   testthat::expect_equal(out,docker_org)
-  
-  ## Infer from GH info in DESCRIPTION
-  d <- desc::desc(package = "yaml")
-  out <- infer_docker_org(docker_org = NULL, 
-                          docker_registry = "ghcr.io",
-                          paths=d)
-  testthat::expect_equal(out,"vubiostat")
-  
+
   ## Unable to infer for DockerHub
   testthat::expect_error(
     infer_docker_org(docker_org = NULL, 
